4adec9b891ba44eeb5ea9fec820a7cde8cf447fc
nishi240masa <masa2401106@gmail.com>
2025-02-07 17:49:21 +0900

    Refactor device.go: Update game over logic to include GameOver flag

--- src/services/device.go
+++ src/services/device.go
@@ -118,7 +118,7 @@ func HttpProcessInputFromDevice(deviceID, action, state string) error {
 	}
 
 	// ゲームオーバーの場合、ゲームを終了
-	if target.HP == 0 || attacker.HP == 0 {
+	if target.HP == 0 || attacker.HP == 0  || GameOver {
 		log.Printf("Player %s wins!", attacker.ID)
 
 		if target.HP == 0 {
